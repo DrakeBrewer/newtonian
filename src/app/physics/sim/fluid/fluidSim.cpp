5d6276a1e232ee9e4ab7c2a2992d7e955149303c
Pmedvedskiy <pmedvedskiy1@gmail.com>
2025-02-07 13:34:15 -0800

    Added fluid dir and user input

---
+++ src/app/physics/sim/fluid/fluidSim.cpp
@@ -0,0 +1,53 @@
+#include "fluidSim.h"
+
+int main(int argc, char *argv[]){
+    
+    cout << "Fluid Simulation\n\n";
+
+    cout << "Enter \"help\" to see the options" << endl;
+
+    string userInput = {};
+    while(true){
+        cout << "\nEnter a prompt :";
+        getline(cin,userInput);
+
+        if(userInput.compare("exit") == 0){
+            cout << "Exiting...\n";
+            break;
+        }
+        else if(userInput == "help"){
+            cout << "Prompt options\n"
+                    "exit: will exit the program\n"
+                    "test1: sim an object falling into water\n"
+                    "test2: sim an object starting under the water\n"
+                    "details: detailed explanation of each test" << endl;
+            continue;
+        }
+        else if(userInput == "details"){
+            cout << "Detailed Explanation\n"
+                    "test1:\n" 
+                    "   Object starts at a height of 10 meters and with a velocity of 0\n"
+                    "test2:\n"
+                    "   sim an object starting under the water\n";
+            continue;
+        }
+        else if(userInput.compare("test1") == 0){
+            cout << "Don't have the sim ready yet" << endl;
+            //simulate test1
+            continue;
+        }
+        else if(userInput.compare("test2") == 0){
+            cout << "Don't have the sim ready yet" << endl;
+            //simulate test2
+            continue;
+        }
+        else{
+            cout << "Not a prompt, do \"help\" for prompts" << endl;
+            continue;
+        }
+
+        break;
+    }
+
+    return 0;
+}
---
+++ src/app/physics/sim/fluid/fluidSim.h
@@ -0,0 +1,12 @@
+#ifndef FLUIDSIM_H
+#define FLUIDSIM_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+
+#endif
