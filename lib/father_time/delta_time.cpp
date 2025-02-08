9cea9c7436ce230d065395d02567f091f8cede60
drakebrewer <drake@netstorms.com>
2025-02-07 18:48:36 -0800

    feat: add delta time function

---
+++ lib/father_time/delta_time.cpp
@@ -0,0 +1,17 @@
+#include <ctime>
+#include <chrono>
+
+#include "delta_time.hpp"
+
+/*
+ * delta_time
+ * @brief Calculates the delta time in order for frame independent state increments
+ * @return time since last call to function
+ */
+double delta_time() {
+	// Removes the need to track things with a class
+	static auto prev_time = std::chrono::steady_clock::now();
+	auto curr_time = std::chrono::steady_clock::now();
+
+	return std::chrono::duration<double>(curr_time - prev_time).count();
+}
