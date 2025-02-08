93807d01eeecd25bf30ccf78718629957a2198d3
pmedvedskiy <p.medvedskiy1@gmail.com>
2025-02-07 23:56:11 -0800

    Created a simple test simulation

--- src/simulations/fluidSim/fluidSim.cpp
+++ src/simulations/fluidSim/fluidSim.cpp
@@ -1,4 +1,4 @@
-#include "fluidSim.h"
+#include "fluidSim.hpp"
 
 int main(int argc, char *argv[]){
     
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]){
         cout << "\nEnter a prompt :";
         getline(cin,userInput);
 
-        if(userInput.compare("exit") == 0){
+        if(userInput.compare("exit") == 0 || userInput == "q"){
             cout << "Exiting...\n";
             break;
         }
@@ -28,18 +28,20 @@ int main(int argc, char *argv[]){
                     "test1:\n" 
                     "   Object starts at a height of 10 meters and with a velocity of 0\n"
                     "test2:\n"
-                    "   sim an object starting under the water\n";
+                    "   Object starts at a height of -10 meters (in the water)\n"
+                    "   and a velocity of 0\n";
             continue;
         }
         else if(userInput.compare("test1") == 0){
             cout << "Don't have the sim ready yet" << endl;
-            //simulate test1
+            runTest1();
             continue;
         }
         else if(userInput.compare("test2") == 0){
             cout << "Don't have the sim ready yet" << endl;
+            
             //simulate test2
-
+            //runTest2();
             continue;
         }
         else{
@@ -55,8 +57,30 @@ int main(int argc, char *argv[]){
 
 
 int runTest1(){
-    
+    //The top of the water level is at z=0 , everything less than 0 is water
 
+    string input;
+    int sec = 0;
+    int vFin = 0;
+
+    object obj(10);
+    fluid water;
+
+    // Will add user input to set the initial height, water level, velocity, density
+
+
+    //Will need to add the real equations for gravity to get the actual values of the final
+    //velocity of the object before it enters the water
+
+    while(obj.getZ() > water.getFL()){
+        obj.setZ((obj.getZ() - 9.81));
+        vFin = obj.getInitV() + (9.81*sec);
+        sec++;
+        cout<<"Sec:" << sec << "\nObject height:" << obj.getZ()<< endl;
+    }
+
+    cout<<"Simulation Done\n";
+    cout<<"Object reached the water in "<< sec << " seconds." << endl;
 
 
     return 0;
@@ -66,6 +90,8 @@ int runTest1(){
 int runTest2(){
     
     //Not implemented yet
+    //
+    //Might not need it, think I can implement both into 1 test
 
     return 0;
 }
