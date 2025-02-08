43556a1c18cb099c01d6bedf8149d69f395cb6e0
collections54 <potatomaniscrazy@gmail.com>
2025-02-07 19:33:58 -0800

    Brendan/qt examples (#73)
    * Added example qt project
    
    * Added example qt project
    
    * Update window.cpp
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update CMakeLists.txt

---
+++ examples/qt_examples/window.cpp
@@ -0,0 +1,15 @@
+// Code Sourced from QT Widgets Tutorial - Creating a Window
+// https://doc.qt.io/qt-6/qtwidgets-tutorials-widgets-toplevel-example.html
+// Creates a 320x240 window called toplevel
+#include <QtWidgets>
+
+int main(int argc, char * argv[])
+{
+    QApplication app(argc, argv);
+    QWidget window;
+    window.resize(320,240);
+    window.show();
+    window.setWindowTitle(
+        QApplication::translate("toplevel", "Top-level widget"));
+    return app.exec();
+}
