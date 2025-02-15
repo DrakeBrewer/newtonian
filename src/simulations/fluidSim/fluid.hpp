e18d063a5d7905327fccb60071ed288632f2397d
pmedvedskiy <p.medvedskiy1@gmail.com>
2025-02-14 23:52:21 -0800

    Split header, added to constructors, added second test, added comments for future update

---
+++ src/simulations/fluidSim/fluid.hpp
@@ -0,0 +1,55 @@
+#ifndef FLUID_H
+#define FLUID_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class fluid{
+    float density; //kg/m^3
+    float fluidLevel; // top of the fluid
+    float viscosity;
+    float pressure;
+    
+    public:
+        // Constructors
+        fluid(){
+            density = 0;
+            fluidLevel = 0;
+            viscosity = 0;
+            pressure = 0;
+        }
+
+        fluid(const string liquid) : fluid(){
+            if(liquid == "water"){
+                density = 1000;
+            }else if(liquid == "oil"){ //diesel
+                density = 800;
+            }else if(liquid == "corn syrup"){
+                density = 1380;
+            }
+            //Default
+        }
+        
+        fluid(const string liquid, float fluidLevel) : fluid(liquid){ 
+            this->fluidLevel = fluidLevel;
+        }
+        
+        // Setters and Getters
+
+        void setDensity(float d){density = d;}
+        void setFL(float fl){fluidLevel = fl;}
+        void setViscosity(float v){viscosity = v;}
+        void setPressure(float p){pressure = p;}
+        float getDensity(){ return density;}
+        float getFL(){ return fluidLevel;}
+        float getViscosity(){ return viscosity;}
+        float getPressure(){ return pressure;}
+};
+
+// Prototypes
+
+int runTest1();
+int runTest2();
+
+#endif
