7dc5f69dc7891f45b39c60b208af18ca1dae7eb5
drakebrewer <drake@netstorms.com>
2025-02-21 22:45:33 -0800

    feat: rendered styled window fix: accidental deletion of cmake

--- src/simulations/collisionSim/main.cpp
+++ src/simulations/collisionSim/main.cpp
@@ -8,14 +8,8 @@ int main(int argc, char **argv) {
 	std::cout << "Hello World!" << std::endl;
 	QApplication app(argc, argv);
 
-	// TODO: Render a window
-	// QWidget window;
-
 	MainWindow mw;
 	mw.show();
 
-	// window.resize(500, 500);
-	// window.show();
-	// window.setWindowTitle(QApplication::translate("test", "my-test"));
 	return app.exec();
 }
--- src/simulations/collisionSim/window.hpp
+++ src/simulations/collisionSim/window.hpp
@@ -1,14 +1,20 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QtWidgets/qwidget.h>
 
 class MainWindow : public QMainWindow {
 	Q_OBJECT
-// TODO: add privates for toolbars & stuff
-// private:
-	// GlView *physicsView;
 
 public:
 	MainWindow(QWidget *parent = nullptr);
 	~MainWindow();
 };
+
+// TODO: Turn sidebar into a class
+// class Sidebar : public QWidget {
+// 	Q_OBJECT
+
+// public:
+// 	Sidebar(QWidget *parent = nullptr);
+// };
