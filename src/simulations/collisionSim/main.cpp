fa181d7936c18071c00b64aa4b385944e63186db
drakebrewer <drake@netstorms.com>
2025-02-21 22:38:37 -0800

    feat: start up main window

--- src/simulations/collisionSim/main.cpp
+++ src/simulations/collisionSim/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <QWidget>
+#include <QApplication>
 
-int main() {
+#include "window.hpp"
+
+int main(int argc, char **argv) {
 	std::cout << "Hello World!" << std::endl;
+	QApplication app(argc, argv);
 
-	return 0;
+	// TODO: Render a window
+	// QWidget window;
+
+	MainWindow mw;
+	mw.show();
+
+	// window.resize(500, 500);
+	// window.show();
+	// window.setWindowTitle(QApplication::translate("test", "my-test"));
+	return app.exec();
 }
