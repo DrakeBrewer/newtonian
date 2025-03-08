e3c570073e8a5d5066ab1ec47220845987a32d65
Drake Brewer <drake@netstorms.com>
2025-03-07 22:18:06 -0800

    feat: add basic shape classes (#102)
    * feat: add classes for basic 3d shapes
    
    * feat: add vertex calcs to cuboid constructor
    
    * feat: add vertex updating for cube and made update virtual
    
    * fix: shape interface methods not properly initialized

--- src/simulations/collisionSim/rigidBody.cpp
+++ src/simulations/collisionSim/rigidBody.cpp
@@ -48,7 +48,10 @@ void RigidBody::update(double delta) {
 		this->position.z += this->velocity.z * delta;
 	}
 
-	std::cout << "\r" << std::fflush(stdout) << "x: " << this->position.x << "y: " << this->position.y << "z: " << this->position.z;
+	std::cout << "\r" << std::fflush(stdout) <<
+		"x: " << this->position.x <<
+		", y: " << this->position.y <<
+		", z: " << this->position.z;
 }
 
 
@@ -57,4 +60,99 @@ void RigidBody::applyForce(Vector3d force) {
 	this->acceleration.x = force.x / this->mass;
 	this->acceleration.y = force.y / this->mass;
 	this->acceleration.z = force.z / this->mass;
+}
+
+
+Sphere::Sphere(float radius, Vector3d position, float mass)
+	: RigidBody(position, Vector3d(), Vector3d(), mass, false) {
+	this->radius = radius;
+	this->position = position;
+	this->mass = mass;
+}
+
+float Sphere::surfaceArea() {
+	return 4.0f * PI * std::pow(this->radius, 2);
+}
+
+float Sphere::volume() {
+	return (4.0f/3.0f) * PI * std::pow(this->radius, 3);
+}
+
+float Sphere::diameter() {
+	return 2 * this->radius;
+}
+
+Cuboid::Cuboid(float length, float width, float height, Vector3d position, float mass)
+	: RigidBody(position, Vector3d(), Vector3d(), mass, false) {
+	this->length = length;
+	this->width = width;
+	this->height = height;
+
+	float halfL = this->length / 2;
+	float halfW = this->width / 2;
+	float halfH = this->height / 2;
+
+	this->vertices[0] = Vector3d(this->position.x + halfL, this->position.y + halfW, this->position.z + halfH);  // top-front-right
+	this->vertices[1] = Vector3d(this->position.x - halfL, this->position.y + halfW, this->position.z + halfH);  // top-front-left
+	this->vertices[2] = Vector3d(this->position.x + halfL, this->position.y - halfW, this->position.z + halfH);  // top-back-right
+	this->vertices[3] = Vector3d(this->position.x - halfL, this->position.y - halfW, this->position.z + halfH);  // top-back-left
+	this->vertices[4] = Vector3d(this->position.x + halfL, this->position.y + halfW, this->position.z - halfH);  // bottom-front-right
+	this->vertices[5] = Vector3d(this->position.x - halfL, this->position.y + halfW, this->position.z - halfH);  // bottom-front-left
+	this->vertices[6] = Vector3d(this->position.x + halfL, this->position.y - halfW, this->position.z - halfH);  // bottom-back-right
+	this->vertices[7] = Vector3d(this->position.x - halfL, this->position.y - halfW, this->position.z - halfH);  // bottom-back-left
+}
+
+void Cuboid::update(double delta) {
+	RigidBody::update(delta);
+	this->updateVertices();
+}
+
+void Cuboid::updateVertices() {
+	float halfL = this->length / 2;
+	float halfW = this->width / 2;
+	float halfH = this->height / 2;
+
+	// NOTE: This code is disgusting
+	// TODO: Find a not gross way to do it without remaking a bunch of vectors..?
+	this->vertices[0].x = this->position.x + halfL;
+	this->vertices[0].y = this->position.y + halfW;
+	this->vertices[0].z = this->position.z + halfH;
+
+	this->vertices[1].x = this->position.x - halfL;
+	this->vertices[1].y = this->position.y + halfW;
+	this->vertices[1].z = this->position.z + halfH;
+
+	this->vertices[2].x = this->position.x + halfL;
+	this->vertices[2].y = this->position.y - halfW;
+	this->vertices[2].z = this->position.z + halfH;
+
+	this->vertices[3].x = this->position.x - halfL;
+	this->vertices[3].y = this->position.y - halfW;
+	this->vertices[3].z = this->position.z + halfH;
+
+	this->vertices[4].x = this->position.x + halfL;
+	this->vertices[4].y = this->position.y + halfW;
+	this->vertices[4].z = this->position.z - halfH;
+
+	this->vertices[5].x = this->position.x - halfL;
+	this->vertices[5].y = this->position.y + halfW;
+	this->vertices[5].z = this->position.z - halfH;
+
+	this->vertices[6].x = this->position.x + halfL;
+	this->vertices[6].y = this->position.y - halfW;
+	this->vertices[6].z = this->position.z - halfH;
+
+	this->vertices[7].x = this->position.x - halfL;
+	this->vertices[7].y = this->position.y - halfW;
+	this->vertices[7].z = this->position.z - halfH;
+}
+
+float Cuboid::surfaceArea() {
+	return 2 * (this->length * this->width +
+		    this->height * this->width +
+		    this->length * this->height);
+}
+
+float Cuboid::volume() {
+	return this->length * this->width * this->height;
 }
--- src/simulations/collisionSim/rigidBody.hpp
+++ src/simulations/collisionSim/rigidBody.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
+#define PI 3.14f
+
+// TODO: Evaluate the need for doubles instead of floats in 
+// some of the more critical sections of the sim
+
+class IShape {
+public:
+	IShape() {};
+	virtual ~IShape() {};
+
+	virtual float volume() = 0;
+	virtual float surfaceArea() = 0;
+};
+
 class Vector3d {
 public:
-	Vector3d(): x(0), y(0), z(0) {}
+	Vector3d(): x(0), y(0), z(0) {};
 	Vector3d(float x, float y, float z);
 	~Vector3d(){};
 
@@ -10,8 +24,6 @@ public:
 	float y;
 	float z;
 
-	// TODO: vector params should be by ref since we prob
-	// aren't changing the incoming vector.
 	Vector3d crossProduct(const Vector3d& vec);
 	float dotProduct(const Vector3d& vec);
 	float magnitude();
@@ -31,8 +43,46 @@ public:
 	Vector3d velocity;
 	Vector3d acceleration;
 	float mass;
+	// TODO: implement this with quaternions
+	// float rotation;
+
 	bool isStatic;
 
-	void update(double delta);
+	virtual void update(double delta);
 	void applyForce(Vector3d force);
 };
+
+class Sphere : public RigidBody,
+	public IShape {
+public:
+	Sphere(float radius, Vector3d position, float mass);
+	~Sphere() {};
+
+	float radius;
+
+	float surfaceArea();
+	float volume();
+	float diameter();
+};
+
+class Cuboid : public RigidBody,
+	public IShape {
+public:
+	Cuboid(float length, float width, float height, Vector3d position, float mass);
+	~Cuboid() {};
+
+	float length;
+	float width;
+	float height;
+
+	Vector3d vertices[8];
+
+	void update(double delta);
+
+	float surfaceArea();
+	float volume();
+private:
+	void updateVertices();
+};
+
+// TODO: meshes and compound objects?
