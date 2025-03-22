4453e1da1e964064feb096512712ac00552e1dbe
Drake Brewer <drake@netstorms.com>
2025-03-21 22:12:54 -0700

    feat!: add rigid body, world, and vector3d libraries (#114)
    BREAKING CHANGE: affected libraries may have new paths or require a different library to be added
    
    * feat: add rigid body and world cmake libraries
    
    * fix: update test cmake
    
    * feat: add vector3d library
    
    * cleanup: move vector3d unit tests

--- src/physics/rigidBody/rigidBody.hpp
+++ src/physics/rigidBody/rigidBody.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "vector3d.hpp"
 
 #define PI 3.14f
 
@@ -12,27 +13,6 @@ public:
 
 	virtual float volume() = 0;
 	virtual float surfaceArea() = 0;
-};
-
-class Vector3d {
-public:
-	Vector3d(): x(0), y(0), z(0) {};
-
-	Vector3d(float x, float y, float z);
-	~Vector3d(){};
-
-	float x;
-	float y;
-	float z;
-
-	Vector3d crossProduct(const Vector3d& vec);
-	float dotProduct(const Vector3d& vec);
-	float magnitude();
-
-	// TODO:: https://people.math.harvard.edu/~jjchen/math21a/handouts/vector-ops.html
-	// - [ ] Vector addition
-	// - [ ] Scalar multiplication
-	// - [ ] Transformations?
 };
 
 class RigidBody {
