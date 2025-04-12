64a312c334ccefa8d4732027b1d0fcaf57a19979
Phillip Medvedskiy <163092467+Pmedvedskiy@users.noreply.github.com>
2025-04-12 00:52:41 -0700

    Cleaned up the fluid dir and began implementing rigidBody with my simulation (#137)

--- src/simulations/fluidSim/fluidSim.hpp
+++ src/simulations/fluidSim/fluidSim.hpp
@@ -2,10 +2,12 @@
 #include "fluid.hpp"
 #include "object.hpp"
 
+void userInterface();
 
 int run();
 int runTest1();
 int runTest2();
-float netForce(object obj, fluid liquid, float velocity);
-void userInterface();
+
+// Need: to implement rigidBody
 int runSimulation(object obj, fluid liquid, bool printFlag);
+float netForce(object obj, fluid liquid, float velocity);
