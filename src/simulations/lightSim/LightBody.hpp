aaf641418359ae17753bce6fdaa740bebb104c51
tboenish <154274875+tboenish@users.noreply.github.com>
2025-04-20 15:26:51 -0700

    implemented rigid body (#141)
    * implemented rigid body

--- src/simulations/lightSim/LightBody.hpp
+++ src/simulations/lightSim/LightBody.hpp
@@ -1,1 +1,24 @@
+#pragma once
+
+#include <cmath>
 #include "rigidBody.hpp"
+
+class LightBody : public RigidBody {
+public:
+    LightBody(Vector3d origin, Vector3d direction, float wavelength, float speed = 2.997925E8);
+    ~LightBody() = default; 
+
+    float wavelength;
+    float speed;
+    Vector3d direction;
+
+    // Refractive index of medium currently in
+    float refractiveIndex = 1.0f;
+
+    void update(double delta) override;
+    void reflect(Vector3d& surfaceNormal);
+    void refract(Vector3d& surfaceNormal, float newRefractiveIndex);
+
+    std::unordered_map<std::string, float> getAttributes() const override;
+
+};
---
+++ src/simulations/planetSim/simulation.hpp
@@ -0,0 +1,6 @@
+
+#pragma once
+using namespace std;
+
+#include<string>
+void runSimulation(float grav, float x, float y, float z,float xPLane, float yPlane, float zPlane, float initV, float mass,string planetName);
